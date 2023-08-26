8b24198e0bb8d8749325fed27acf9f9fc37643e0
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-08-25 23:03:49 -0500

    Resolucion ejercicio

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -21,12 +21,14 @@ const UserContext = ({ children }) => {
   //GUIARSE DE AQUÍ
   //   const newProducts = productsAdded.filter((product) => product.id !== productInfo.id)
 
-  console.log(allProducts)
-
   //1. Traer los diez primeros productos del 'AllProducts'
   //2. Filtrar todos los productos que tengan un precio menor a 100
   //3. Filtrar todos los productor que estén en la categoría de 'men\'s clothing'
 
+  const ejercicio1 = allProducts.filter((product, index) => index < 10)
+  const ejercicio2 = allProducts.filter(product => product.price < 100 )
+  const ejercicio3 = allProducts.filter(product => product.category === 'men\'s clothing')
+  
   return (
     <ContextShop.Provider value={{ allProducts, setAllProducts, total, setTotal, productsAdded, setProductsAdded }}>
       {children}
