1ad9b848b3617c2be8b4fa79813d70245d147910
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2023-08-25 22:54:19 -0500

    Ejercicios

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -8,7 +8,7 @@ const UserContext = ({ children }) => {
   const [productsAdded, setProductsAdded] = useState([])
 
   const getProducts = async () => {
-    const fetchApi = await fetch(`https://fakestoreapi.com/products?limit=10`)
+    const fetchApi = await fetch(`https://fakestoreapi.com/products?limit=20`)
     const response = await fetchApi.json()
 
     setAllProducts(response)
@@ -18,6 +18,15 @@ const UserContext = ({ children }) => {
     getProducts()
   }, [])
   
+  //GUIARSE DE AQUÍ
+  //   const newProducts = productsAdded.filter((product) => product.id !== productInfo.id)
+
+  console.log(allProducts)
+
+  //1. Traer los diez primeros productos del 'AllProducts'
+  //2. Filtrar todos los productos que tengan un precio menor a 100
+  //3. Filtrar todos los productor que estén en la categoría de 'men\'s clothing'
+
   return (
     <ContextShop.Provider value={{ allProducts, setAllProducts, total, setTotal, productsAdded, setProductsAdded }}>
       {children}
